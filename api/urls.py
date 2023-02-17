1034725678c89f2968cc162672c0407547617d25
PowerLichen <ms.choe015@gmail.com>
2023-02-17 17:09:16 +0900

    Add simplejwt package and url path

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
+    path("auth/", include("api.auth.urls")),
     path('routine/', include('api.routine.urls')),
 ]
--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,10 @@ AUTHENTICATION_BACKENDS = [
 
 
 # Django rest framework definition
-REST_FRAMEWORK = {
+REST_FRAMEWORK = {    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
